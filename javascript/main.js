0959518f64152b1902782b53fa467aefbbfc0fff
reshovroy <reshovr58@gmail.com>
2018-04-06 10:40:22 +0530

    newsbar added

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,10 @@
 $(document).ready(function(){
   $('.parallax').parallax();
+  $('.button-collapse').sideNav();
   $('.slider').slider();
-
-  $('.tabs').tabs();
-
-  $('.sidenav').sidenav();
-
-  $('.sidenav-trigger').click(function(){
-    $('.sidenav').sidenav('close');
-  });
-
+  $('.btn').sideNav();
 });
 
-// rendering google maps
-
-function initMap() {
-  var uluru = {lat: 29.948, lng: 76.817};
-  var map = new google.maps.Map(document.querySelector('#map'), {
-    zoom: 15,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
 
 //            own javascript
 
@@ -35,13 +15,13 @@ function scroller() {
     var ypos = window.pageYOffset;
     if(ypos >= go)
         {
-            // navbar.style.background="#c8e6c9a";
+            navbar.style.background="white";
             navbar.style.position="fixed";
             navbar.style.top="0";
         }
     else
         {
-            // navbar.style.background="#fffa";
+            navbar.style.background="transparent";
             navbar.style.position="absolute";
             navbar.style.top=go+"px";
         }
